f3ad4f990707b812c822e542ad35b2921f2fc4c6
Sarbagya Dhaubanjar <sarbagyastha@gmail.com>
2023-07-09 12:45:26 +0545

    :recycle: added form controller base

--- lib/src/core/form_controller.dart
+++ lib/src/core/form_controller.dart
@@ -5,59 +5,17 @@
 import 'package:flutter/foundation.dart';
 import 'package:uniform/src/core/input_form_state.dart';
 
+part 'form_controller_base.dart';
 part 'field_controller.dart';
 part 'input_field_error.dart';
 part 'input_field_validator.dart';
 
 /// A controller for a form.
-class FormController extends ChangeNotifier {
+class FormController extends _FormControllerBase {
   /// Creates an instance of [FormController].
-  FormController({
-    Set<InputFieldValidator> validators = const {},
-  }) : _validators = validators;
+  FormController({super.validators});
 
-  final Map<Object, FieldController> _fields = {};
-  final Set<InputFieldValidator> _validators;
-  final Set<InputFormState> _states = {
-    InputFormState.pristine,
-    InputFormState.untouched,
-  };
-  final Map<Object, InputFieldError> _errors = {};
-  final Set<Object> _activeTags = {};
-
-  /// The current [errors] of this form.
-  Map<Object, InputFieldError> get errors => Map.unmodifiable(_errors);
-
-  /// All the field [tags] bound to this form.
-  Set<Object> get tags => Set.unmodifiable(_fields.keys);
-
-  /// All the field [tags] active in the form.
-  Set<Object> get activeTags => Set.unmodifiable(_activeTags);
-
-  /// The current [states] of this form.
-  Set<InputFormState> get states => Set.unmodifiable(_states);
-
-  /// The current [values] of this form.
-  Map<Object, Object?> get values {
-    return Map.fromEntries(
-      _fields.entries.map((e) => MapEntry(e.key, e.value.value)),
-    );
-  }
-
-  /// Returns true if the form has been submitted.
-  bool get isSubmitted => states.contains(InputFormState.submitted);
-
-  /// Gets the [FieldController] bound with the [tag].
-  FieldController<T> call<T extends Object>(Object tag) {
-    return getField(tag);
-  }
-
-  /// Returns true if the form states contains any of the [states].
-  bool contains(Set<InputFormState> states) {
-    return _states.intersection(states).isNotEmpty;
-  }
-
-  /// Gets the [FieldController] bound with the [tag].
+  @override
   FieldController<T> getField<T extends Object>(Object tag) {
     assert(
       _fields.containsKey(tag),
@@ -66,17 +24,20 @@ class FormController extends ChangeNotifier {
     return _fields[tag]! as FieldController<T>;
   }
 
-  /// Gets the value of the field bound with the [tag].
+  @override
+  FieldController<T> call<T extends Object>(Object tag) => getField(tag);
+
+  @override
+  bool contains(Set<InputFormState> states) {
+    return _states.intersection(states).isNotEmpty;
+  }
+
+  @override
   T? getValue<T extends Object>(Object tag) {
     return getField(tag).value as T?;
   }
 
-  /// Validates the form.
-  ///
-  /// If [tags] is not null,
-  /// only the fields with the given [tags] will be validated.
-  /// If [notify] is true,
-  /// the form will notify its listeners if the form is invalid.
+  @override
   bool validate({Set<Object>? tags, bool notify = true}) {
     final fields = (tags ?? _activeTags).map(getField);
 
@@ -90,13 +51,13 @@ class FormController extends ChangeNotifier {
     return isFormValid;
   }
 
-  /// Sets the field bound with the [tag] as active.
+  @override
   void activate(Object tag) => _activeTags.add(tag);
 
-  /// Sets the field bound with the [tag] as inactive.
+  @override
   void deactivate(Object tag) => _activeTags.remove(tag);
 
-  /// Removes the field bound with the [tag].
+  @override
   void remove(Object tag) {
     final controller = _fields.remove(tag);
     _activeTags.remove(tag);
@@ -105,7 +66,7 @@ class FormController extends ChangeNotifier {
     controller?.dispose();
   }
 
-  /// Resets the form to initial state.
+  @override
   void reset() {
     for (final field in _fields.values) {
       field.reset();
@@ -126,8 +87,7 @@ class FormController extends ChangeNotifier {
     super.dispose();
   }
 
-  /// Sets the value for [isSubmitted] of the form and each field.
-  // ignore: avoid_positional_boolean_parameters
+  @override
   void setSubmitted(bool isSubmitted) {
     if (isSubmitted) {
       _states.add(InputFormState.submitted);
@@ -142,9 +102,7 @@ class FormController extends ChangeNotifier {
     notifyListeners();
   }
 
-  /// Sets the initial values for the form.
-  ///
-  /// If [notify] is true, each field will be notified with the initial value.
+  @override
   void setInitialValues(
     Map<Object, Object?> initialValues, {
     bool notify = true,
@@ -152,41 +110,5 @@ class FormController extends ChangeNotifier {
     for (final entry in initialValues.entries) {
       getField(entry.key).setInitialValue(entry.value, notify: notify);
     }
-  }
-
-  void _setDirty() {
-    if (_states.contains(InputFormState.pristine)) {
-      _states
-        ..remove(InputFormState.pristine)
-        ..add(InputFormState.dirty);
-      notifyListeners();
-    }
-  }
-
-  void _setTouched() {
-    if (_states.contains(InputFormState.untouched)) {
-      _states
-        ..remove(InputFormState.untouched)
-        ..add(InputFormState.touched);
-      notifyListeners();
-    }
-  }
-
-  void _setValidity({required bool isValid}) {
-    if (isValid && _states.contains(InputFormState.invalid)) {
-      _states
-        ..remove(InputFormState.invalid)
-        ..add(InputFormState.valid);
-      notifyListeners();
-    } else if (!isValid && _states.contains(InputFormState.valid)) {
-      _states
-        ..remove(InputFormState.valid)
-        ..add(InputFormState.invalid);
-      notifyListeners();
-    }
-  }
-
-  void _setError(InputFieldError error) {
-    _errors[error.tag] = error;
   }
 }
---
+++ lib/src/core/form_controller_base.dart
@@ -0,0 +1,118 @@
+part of 'form_controller.dart';
+
+abstract class _FormControllerBase extends ChangeNotifier {
+  _FormControllerBase({
+    Set<InputFieldValidator> validators = const {},
+  }) : _validators = validators;
+
+  final Map<Object, FieldController> _fields = {};
+  final Set<InputFieldValidator> _validators;
+  final Set<InputFormState> _states = {
+    InputFormState.pristine,
+    InputFormState.untouched,
+  };
+  final Map<Object, InputFieldError> _errors = {};
+  final Set<Object> _activeTags = {};
+
+  /// The current [errors] of this form.
+  Map<Object, InputFieldError> get errors => Map.unmodifiable(_errors);
+
+  /// All the field [tags] bound to this form.
+  Set<Object> get tags => Set.unmodifiable(_fields.keys);
+
+  /// All the field [tags] active in the form.
+  Set<Object> get activeTags => Set.unmodifiable(_activeTags);
+
+  /// The current [states] of this form.
+  Set<InputFormState> get states => Set.unmodifiable(_states);
+
+  /// The current [values] of this form.
+  Map<Object, Object?> get values {
+    return Map.fromEntries(
+      _fields.entries.map((e) => MapEntry(e.key, e.value.value)),
+    );
+  }
+
+  /// Returns true if the form has been submitted.
+  bool get isSubmitted => states.contains(InputFormState.submitted);
+
+  /// Gets the [FieldController] bound with the [tag].
+  FieldController<T> call<T extends Object>(Object tag);
+
+  /// Gets the [FieldController] bound with the [tag].
+  FieldController<T> getField<T extends Object>(Object tag);
+
+  /// Returns true if the form states contains any of the [states].
+  bool contains(Set<InputFormState> states);
+
+  /// Gets the value of the field bound with the [tag].
+  T? getValue<T extends Object>(Object tag);
+
+  /// Validates the form.
+  ///
+  /// If [tags] is not null,
+  /// only the fields with the given [tags] will be validated.
+  /// If [notify] is true,
+  /// the form will notify its listeners if the form is invalid.
+  bool validate({Set<Object>? tags, bool notify = true});
+
+  /// Sets the field bound with the [tag] as active.
+  void activate(Object tag);
+
+  /// Sets the field bound with the [tag] as inactive.
+  void deactivate(Object tag);
+
+  /// Removes the field bound with the [tag].
+  void remove(Object tag);
+
+  /// Resets the form to initial state.
+  void reset();
+
+  /// Sets the value for [isSubmitted] of the form and each field.
+  // ignore: avoid_positional_boolean_parameters
+  void setSubmitted(bool isSubmitted);
+
+  /// Sets the initial values for the form.
+  ///
+  /// If [notify] is true, each field will be notified with the initial value.
+  void setInitialValues(
+    Map<Object, Object?> initialValues, {
+    bool notify = true,
+  });
+
+  void _setDirty() {
+    if (_states.contains(InputFormState.pristine)) {
+      _states
+        ..remove(InputFormState.pristine)
+        ..add(InputFormState.dirty);
+      notifyListeners();
+    }
+  }
+
+  void _setTouched() {
+    if (_states.contains(InputFormState.untouched)) {
+      _states
+        ..remove(InputFormState.untouched)
+        ..add(InputFormState.touched);
+      notifyListeners();
+    }
+  }
+
+  void _setValidity({required bool isValid}) {
+    if (isValid && _states.contains(InputFormState.invalid)) {
+      _states
+        ..remove(InputFormState.invalid)
+        ..add(InputFormState.valid);
+      notifyListeners();
+    } else if (!isValid && _states.contains(InputFormState.valid)) {
+      _states
+        ..remove(InputFormState.valid)
+        ..add(InputFormState.invalid);
+      notifyListeners();
+    }
+  }
+
+  void _setError(InputFieldError error) {
+    _errors[error.tag] = error;
+  }
+}
